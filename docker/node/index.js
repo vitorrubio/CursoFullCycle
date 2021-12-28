a9c7ab902cb24ecfb249ce276449db62e4cfac19
vitorrubio <vitorrubio@gmail.com>
2021-12-28 14:27:34 -0300

    dockerfile com dockerize e api post exemplo

--- docker/node/index.js
+++ docker/node/index.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 const config = {
     host: 'db',
     user: 'root',
@@ -12,16 +15,36 @@ const config = {
 
 const mysql = require('mysql');
 const connection = mysql.createConnection(config);
-const sql = 'insert into people (name) values (\'vitor\')';
+const sql = 'insert into people (name) values (\'Andressa\')';
 connection.query(sql);
 connection.end();
 
 
-app.get('/', (req, res) => res.send('<h1>Full Cycle</h1>'));
+app.get('/', (req, res) => res.send('<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd"><html><body><h1>Full Cycle</h1><table> <tr><td>o</td><td>o</td><td rowspan=0>o</td></tr> <tr><td>o</td><td>o</td></tr> <tr><td>o</td><td>o</td></tr> </table></body></html>'));
+
+app.post('/', (req, res) => { 
+
+    console.log(req.body);
+    var nome = req.body.nome;
+
+    const mysql = require('mysql');
+    const connection = mysql.createConnection(config);
+    const sql = `insert into people (name) values ('${nome}')`;
+    connection.query(sql);
+    connection.end();
+
+    res.send('<h1>Cadastrado Com Sucesso</h1>')});
 
 app.listen(
 
-    port, () => console.log(`Example app listening on port ${port}!`)
+    port, () => {
+
+        console.log(`Bem Vindo`)
+        console.log(`Welcome`)
+        console.log(`Benvenuto`)
+        console.log(`Example app listening on port ${port}!`)
+
+    }
 
 )
 
